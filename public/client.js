beff297236a5be0976a75cacbb282886d3bf1110
Rustin Wollin <rwollin96@gmail.com>
2014-09-10 18:00:49 -0400

    resize canvas to fill entire viewport

--- public/client.js
+++ public/client.js
@@ -1,7 +1,22 @@
+function setElementToViewportSize(element) {
+	element.width($(window).innerWidth());
+	element.height($(window).innerHeight());
+}
+
 $(document).ready(function() {
 	var messages = [];
 	var socket = io.connect('http://'+location.host);
+	var canvas = $('#main-view');
 
+	function onWindowResize() {
+		setElementToViewportSize(canvas);
+	}
+
+	window.addEventListener('resize', onWindowResize, false);
+
+	setElementToViewportSize(canvas);
+
+	//socket.IO server message handling
 	socket.on('message', function (data) {
 		alert(data.message);
 	});
