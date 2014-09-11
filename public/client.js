9bbabf64b2d1ea5a4d74bd2e3dc831751df5038b
Rustin Wollin <rwollin96@gmail.com>
2014-09-10 23:57:09 -0400

    generate, transmit and draw rudimentary tile map

--- public/client.js
+++ public/client.js
@@ -1,23 +1,48 @@
+//graphical settings
+var tileSize = 40; //size of each tile in px (will likely end up scaling with viewport size)
+
 function setElementToViewportSize(element) {
-	element.width($(window).innerWidth());
-	element.height($(window).innerHeight());
+	element.attr('width', $(window).innerWidth());
+	element.attr('height', $(window).innerHeight());
+}
+
+function drawMap(map, context){
+	for(var i = 0; i < map.length; i++) {
+		var column = map[i];
+		for(var j = 0; j < column.length; j++) {
+			var x = i * tileSize;
+			var y = j * tileSize;
+			context.rect(x, y, tileSize, tileSize);
+			context.fillText(column[j], x + tileSize * 0.1, y + tileSize * 0.9);
+			context.stroke();
+		}
+	}
 }
 
 $(document).ready(function() {
 	var messages = [];
 	var socket = io.connect('http://'+location.host);
+	var mapData;
 	var canvas = $('#main-view');
+	var ctx2d = canvas[0].getContext('2d');
+	ctx2d.translate(0.5, 0.5);
 
 	function onWindowResize() {
 		setElementToViewportSize(canvas);
+		ctx2d.font = '40px sans-serif';
+		if(mapData !== undefined)
+			drawMap(mapData, ctx2d);
 	}
-
 	window.addEventListener('resize', onWindowResize, false);
-
-	setElementToViewportSize(canvas);
+	onWindowResize();
 
 	//socket.IO server message handling
 	socket.on('message', function (data) {
 		alert(data.message);
+	});
+
+	socket.on('mapData', function (data) {
+		mapData = data;
+		drawMap(mapData, ctx2d);
 	});
 });
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 8080;
@@ -17,6 +16,38 @@ app.use(express.static(__dirname + '/public'));
 global.io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
 
+function createArray(length) {
+	var arr = new Array(length || 0),
+		i = length;
+
+	if (arguments.length > 1) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		while(i--) arr[length-1 - i] = createArray.apply(this, args);
+	}
+
+	return arr;
+}
+
+function generateDungeon(width, height) { //for now, this function just makes a 2d array and fills it with '1'
+	var dungeon = createArray(width, height);
+
+	for(var i = 0; i < dungeon.length; i++) {
+		var column = dungeon[i];
+			for(var j = 0; j < column.length; j++) {
+				column[j] = 1;
+				//console.log("dungeon[" + i + "][" + j + "] = " + column[j]);
+			}
+	}
+
+	return dungeon;
+}
+
+var MAP_WIDTH = 10;
+var MAP_HEIGHT = 10;
+var dungeon = generateDungeon(MAP_WIDTH, MAP_HEIGHT);
+console.log(dungeon);
+
 io.sockets.on('connection', function (socket) {
 	socket.emit('message', { message: 'Welcome to the lobby.' });
+	socket.emit('mapData', dungeon);
 });
