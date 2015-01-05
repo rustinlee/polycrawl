7d8c6fb24dc1163f5b861a4126f7956d77d830b4
Rustin Wollin <rwollin96@gmail.com>
2015-01-05 04:48:38 -0500

    moved Mob constructor + related to lib\mob.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var _und = require('underscore');
 var validator = require('validator');
-var stripJsonComments = require('strip-json-comments');
-var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 8080;
 
+var Mob = require('./lib/mob.js');
+var mobDefinitions = Mob.mobDefinitions;
 var map = require('./lib/map.js');
 var getMap = map.getMap;
 var simulateCombat = require('./lib/combat.js').simulateCombat;
@@ -142,77 +142,6 @@ function gameLoop() {
 	} else {
 		setImmediate(gameLoop);
 	}
-}
-
-var mobDefinitions = JSON.parse(stripJsonComments(fs.readFileSync('./data/mobs.json', 'utf8')));
-var mobID = 0; //probably need to move this to the map
-
-function Mob(template, x, y, color, mapIndex, socketID) {
-	this.mapIndex = mapIndex; //the index of the map the mob is currently on
-	this.fullName = template.fullName;
-	this.symbol = template.symbol;
-	this.color =  color || [255, 255, 255];
-	this.x = x;
-	this.y = y;
-	this.id = mobID;
-	mobID++;
-
-	if (socketID) {
-		this.socketID = socketID;
-	} else {
-		this.socketID = null;
-	}
-
-	//primary stats
-	this.stats = template.stats;
-
-	//secondary (derived) stats
-	this.maxHP = this.stats.con * 10;
-	this.HP = this.maxHP;
-	this.atk = this.stats.str;
-	this.reqAP = Math.floor(30 * (1 * Math.pow(this.stats.agi*125*Math.E, (-this.stats.agi/1250))));
-	this.AP = 0;
-
-	this.limbs = template.limbs;
-
-	var _storedTurn = {};
-
-	this.move = function(x, y) {
-		var targetX = this.x + x;
-		var targetY = this.y + y;
-		if (getMap(this.mapIndex).mapData[targetX][targetY] !== '#') {
-			var mobsAtPosition = getMap(this.mapIndex).getMobsAtPosition(targetX, targetY);
-			if (mobsAtPosition.length === 0) {
-				this.x += x;
-				this.y += y;
-			} else {
-				var targetSocketID = mobsAtPosition[0].socketID;
-				simulateCombat(this, mobsAtPosition[0], getMap(this.mapIndex), this.socketID, targetSocketID);
-			}
-		}
-	};
-
-	this.storeTurn = function(type, data) {
-		_storedTurn = new StoredTurn(type, data);
-	};
-
-	this.hasStoredTurn = function() {
-		return !_und.isEmpty(_storedTurn);
-	};
-
-	this.executeStoredTurn = function() {
-		if (_storedTurn.turnType === 'move') {
-			this.move(_storedTurn.turnData.x, _storedTurn.turnData.y);
-
-			if (this.socketID) {
-				var socket = io.sockets.connected[this.socketID];
-				socket.emit('entitiesData', [getMap(this.mapIndex).getTrimmedGameEntities(), {x: socket.game_player.x, y: socket.game_player.y}]);
-				socket.broadcast.emit('entitiesData', [getMap(this.mapIndex).getTrimmedGameEntities()]);
-			}
-		}
-
-		_storedTurn = {};
-	};
 }
 
 var mapSize = 2500; //number of walkable tiles in the final map
